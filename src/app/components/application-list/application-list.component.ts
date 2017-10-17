d005e3c47e8baf215065c794aee1ba1df369542d
Dhruv Bhanushali <dhruv_b@live.com>
2017-10-18 02:05:50 +0530

    Create the home page which lists all apps open for testing

--- src/app/components/application-list/application-list.component.ts
+++ src/app/components/application-list/application-list.component.ts
@@ -1,15 +1,20 @@
 import {Component, Input} from '@angular/core';
 import {Application} from '../../models/application';
+import {ApplicationSelectionService} from '../../services/application-selection.service';
 
 @Component({
   selector: 'app-application-list',
   templateUrl: './application-list.component.html',
-  styleUrls: ['./application-list.component.css']
+  styleUrls: ['./application-list.component.css'],
 })
 
 export class ApplicationListComponent {
   @Input() applications: Application[];
 
-  constructor() {
+  applicationSelected(application: Application): void {
+    this.applicationSelectionService.selectApplication(application);
+  }
+
+  constructor(private applicationSelectionService: ApplicationSelectionService) {
   }
 }
